5a79c72ea73d1fce25293773844e751a9e216f85
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro.local>
2018-02-21 21:48:56 +0100

    add instance infra

---
+++ infrastructure/deploy_instance.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "test_app" {
+  ami = "ami-af0fc0c0"
+  key_name = "twitter_key"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  provisioner "remote-exec" {
+    inline = [
+      "$ sudo pip install --upgrade awscli",
+      "$ export PATH=/home/ec2-user/.local/bin:$PATH",
+      "$ aws --version",
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user",
+      "aws s3 cp ${aws_s3_bucket.s3_bucket_deploy_artefact.bucket}/${aws_s3_bucket_object.deploy_artefact.key} .",
+      "docker load < ${aws_s3_bucket_object.deploy_artefact.key}.tar.gz"
+    ]
+  }
+
+}
