7cc47f75f6f82cfef3370a095c0ec95886683f72
Alessandro Furkim <alessandrofurkim@Alessandros-MacBook-Pro-2.local>
2018-04-19 22:53:25 +0200

    path fix

--- infrastructure/deploy_artefact_storage.tf
+++ infrastructure/deploy_artefact_storage.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "s3_bucket_deploy_artefact" {
 
 }
 resource "aws_s3_bucket_object" "deploy_artefact" {
-  source = "./${var.git_project}.tar.gz"
+  source = "../${var.git_project}.tar.gz"
   bucket = "${aws_s3_bucket.s3_bucket_deploy_artefact.id}"
   key = "${var.stack}-${var.git_project}/${var.version}.tar.gz"
 }
